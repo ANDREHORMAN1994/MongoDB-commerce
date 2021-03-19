998ec3219cc0b67c8b3338822602d6a52f7eca36
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-03-19 14:13:56 -0300

    erros eslint - 19/03/2021

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,23 +1,6 @@
-db.produtos.updateMany(
-  {
-    nome: "Big Mac",
-  },
-  {
-    $currentDate: {
-      ultimaModificacao: true,
-    },
-  },
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $set: { ultimaModificacao: Date() } },
 );
 
-db.produtos.find(
-  {
-    ultimaModificacao: {
-      $exists: true,
-    },
-  },
-  {
-    _id: 0,
-    nome: 1,
-    ultimaModificacao: 1,
-  },
-);
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
